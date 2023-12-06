c6e0163469a7dceac2dd48f19f03bde6dad1c252
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-12-05 20:17:37 -0800

    Select checkbox elements with JavaScript

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 const sliderFill = document.getElementById("fill");
 const charaterLength = document.getElementById("charaterLength");
 
+const uppercaseCheckbox = document.getElementById("uppercaseCheckbox");
+const lowerCheckCheckbox = document.getElementById("lowercaseBox");
+const numberCheckbox = document.getElementById("numbersCheckbox");
+const symbolsCheckbox = document.getElementById("symbolsCheckbox");
+
 function slider(range) {
     const width = `${parseInt(range.value) * 4.95}%`;
     sliderFill.style.width = width;
