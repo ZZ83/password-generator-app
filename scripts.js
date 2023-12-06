89ef655a2848cdc9b51c41bfac7a41da624c6fdc
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-12-05 20:05:22 -0800

    Fix slider function to include 0

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const sliderFill = document.getElementById("fill");
 const charaterLength = document.getElementById("charaterLength");
 
 function slider(range) {
-    const width = `${(parseInt(range.value) - 1) * 5}%`;
+    const width = `${parseInt(range.value) * 4.95}%`;
     sliderFill.style.width = width;
     charaterLength.innerText = range.value;
 }
