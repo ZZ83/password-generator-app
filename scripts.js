c70b84316a6766d7792c3715cc8a73580952ae95
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-12-06 03:11:48 -0800

    Add uppercase and lowercase alphabet arrays

--- scripts.js
+++ scripts.js
@@ -9,6 +9,63 @@ const lowerCheckCheckbox = document.getElementById("lowercaseBox");
 const numberCheckbox = document.getElementById("numbersCheckbox");
 const symbolsCheckbox = document.getElementById("symbolsCheckbox");
 
+const uppercaseAlphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+];
+const lowercaseAlphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+];
+
 function slider(range) {
     const width = `${parseInt(range.value) * 4.95}%`;
     sliderFill.style.width = width;
