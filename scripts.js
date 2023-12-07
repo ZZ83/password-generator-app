de649bb94db09543820ec444e0487aa78ef58233
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-12-06 20:35:45 -0800

    Remove unused code

--- scripts.js
+++ scripts.js
@@ -11,66 +11,6 @@ const symbolsCheckbox = document.getElementById("symbolsCheckbox");
 
 const generateButton = document.getElementById("generateButton");
 
-const uppercaseAlphabet = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-];
-const lowercaseAlphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-];
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const symbols = ["!", "@", "#", "$", "%", "^", "&", "*"];
-
 function slider(range) {
     const width = `${parseInt(range.value) * 4.95}%`;
     sliderFill.style.width = width;
