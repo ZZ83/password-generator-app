6ad9289ad3cb991b6fdc8edead8122aa16f71b9e
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-12-11 18:19:12 -0800

    Move slider logic back into scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,51 +13,72 @@ const symbolsCheckbox = document.getElementById("symbolsCheckbox");
 
 const generateButton = document.getElementById("generateButton");
 
+const charaterLengthSlider = document.getElementById("charaterLengthSlider");
+const sliderFill = document.getElementById("fill");
+
 copyIcon.addEventListener("click", function () {
     navigator.clipboard.writeText(password.innerText);
 });
 
+charaterLengthSlider.addEventListener("input", function () {
+    const width = `${parseInt(this.value) * 4.95}%`;
+    sliderFill.style.width = width;
+    charaterLength.innerText = this.value;
+});
+
 generateButton.addEventListener("click", function () {
     if (charaterLengthSlider.value !== "0") {
-        let regularX = "";
-        let passwordText = "";
-        let possibleCharacters = "";
-        let passwordLength = charaterLength.innerText;
+        if (
+            uppercaseCheckbox.checked ||
+            lowercaseCheckbox.checked ||
+            numberCheckbox.checked ||
+            symbolsCheckbox.checked
+        ) {
+            let regularX = "";
+            let passwordText = "";
+            let possibleCharacters = "";
+            let passwordLength = charaterLength.innerText;
 
-        if (uppercaseCheckbox.checked) {
-            possibleCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            regularX += "(?=.*[A-Z])";
-        }
-        if (lowercaseCheckbox.checked) {
-            possibleCharacters += "abcdefghijklmnopqrstuvwxyz";
-            regularX += "(?=.*[a-z])";
-        }
-        if (numberCheckbox.checked) {
-            possibleCharacters += "0123456789";
-            regularX += "(?=.*[0-9])";
-        }
-        if (symbolsCheckbox.checked) {
-            possibleCharacters += "!@#$%^&*";
-            regularX += "(?=.*[!@#$%^&*])";
-        }
+            if (uppercaseCheckbox.checked) {
+                possibleCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                regularX += "(?=.*[A-Z])";
+            }
+            if (lowercaseCheckbox.checked) {
+                possibleCharacters += "abcdefghijklmnopqrstuvwxyz";
+                regularX += "(?=.*[a-z])";
+            }
+            if (numberCheckbox.checked) {
+                possibleCharacters += "0123456789";
+                regularX += "(?=.*[0-9])";
+            }
+            if (symbolsCheckbox.checked) {
+                possibleCharacters += "!@#$%^&*";
+                regularX += "(?=.*[!@#$%^&*])";
+            }
 
-        regularX = new RegExp(regularX);
+            regularX = new RegExp(regularX);
 
-        if (parseInt(charaterLength.innerText) >= 4) {
-            while (!regularX.test(passwordText)) {
-                passwordText = "";
+            if (parseInt(charaterLength.innerText) >= 4) {
+                while (!regularX.test(passwordText)) {
+                    passwordText = "";
+                    for (let i = 0; i < passwordLength; i++) {
+                        let randomIndex = Math.floor(Math.random() * possibleCharacters.length);
+                        passwordText += possibleCharacters[randomIndex];
+                    }
+                }
+            } else {
                 for (let i = 0; i < passwordLength; i++) {
                     let randomIndex = Math.floor(Math.random() * possibleCharacters.length);
                     passwordText += possibleCharacters[randomIndex];
                 }
             }
+            password.innerText = passwordText;
+            checkPasswordStrength(passwordText);
         } else {
-            for (let i = 0; i < passwordLength; i++) {
-                let randomIndex = Math.floor(Math.random() * possibleCharacters.length);
-                passwordText += possibleCharacters[randomIndex];
-            }
+            alert("Please select at least one option");
         }
-        password.innerText = passwordText;
-        checkPasswordStrength(passwordText);
     }
 });
+
+if (uppercaseCheckbox.checked || lowercaseCheckbox.checked || numberCheckbox.checked || symbolsCheckbox.checked) {
+}
