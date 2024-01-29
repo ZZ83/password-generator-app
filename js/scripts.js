c8a1d919faf95640dc19f26a48af436cea13fc16
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2024-01-28 20:14:27 -0800

    Delete copyPassword logic

--- js/scripts.js
+++ js/scripts.js
@@ -70,21 +70,3 @@ generateButton.addEventListener("click", function () {
         }
     }
 });
-
-const copied = document.getElementById("toastMessage");
-
-const copyPasswordIcon = document.getElementById("copyPasswordIcon");
-copyPasswordIcon.addEventListener("click", function () {
-    navigator.clipboard.writeText(password.innerText);
-});
-
-copyPasswordIcon.addEventListener("click", function () {
-    navigator.clipboard.writeText(password.innerText);
-    copied.classList.add("fade-out");
-    copied.innerText = "COPIED";
-});
-
-copied.addEventListener("animationend", function () {
-    copied.classList.remove("fade-out");
-    copied.innerText = "";
-});
